0f6b53e01b4ce45ef4204090c9f533ec3a3ee714
intrening <wol87@ya.ru>
2020-01-30 08:33:45 +0300

    optimize funcs

--- main.py
+++ main.py
@@ -5,18 +5,14 @@ import telegram
 import requests
 
 
-def public_vk(file_name, message):
-    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
-    group_id = os.getenv('VK_GROUP_ID')
-    album_id = os.getenv('VK_ALBUM_ID')
-    vk_session = vk_api.VkApi(token=vk_access_token)
+def public_vk(token, group_id, album_id, file_name, message):
+    vk_session = vk_api.VkApi(token=token)
     upload = vk_api.VkUpload(vk_session)
     photo = upload.photo(
         file_name,
         album_id=album_id,
         group_id=group_id,
     )
-
     vk = vk_session.get_api()
     vk.wall.post(
         owner_id=f'-{group_id}',
@@ -25,24 +21,20 @@ def public_vk(file_name, message):
     )
 
 
-def public_telegram(file_name, message):
-    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
-    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
-    bot = telegram.Bot(token=telegram_bot_token)
+def public_telegram(token, chat_id, file_name, message):
+    bot = telegram.Bot(token=token)
     bot.send_message(
-        chat_id=telegram_chat_id,
+        chat_id=chat_id,
         text=message,
     )
-    bot.send_photo(chat_id=telegram_chat_id, photo=open(file_name, 'rb'))
+    bot.send_photo(chat_id=chat_id, photo=open(file_name, 'rb'))
 
 
-def post_facebook(file_name, message):
-    fb_access_token = os.getenv('FB_ACCESS_TOKEN')
-    fb_group_id = os.getenv('FB_GROUP_ID')
-    url = f'https://graph.facebook.com/{fb_group_id}/photos'
+def post_facebook(token, group_id, file_name, message):
+    url = f'https://graph.facebook.com/{group_id}/photos'
     params = {
         'caption': message,
-        'access_token': fb_access_token,
+        'access_token': token,
     }
     files = {
         'upload_file': open(file_name, 'rb'),
@@ -53,6 +45,35 @@ def post_facebook(file_name, message):
 
 def main():
     load_dotenv()
+    vk_access_token = os.getenv('VK_ACCESS_TOKEN')
+    vk_group_id = os.getenv('VK_GROUP_ID')
+    vk_album_id = os.getenv('VK_ALBUM_ID')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    telegram_bot_token = os.getenv('TELEGRAM_BOT_TOKEN')
+    fb_access_token = os.getenv('FB_ACCESS_TOKEN')
+    fb_group_id = os.getenv('FB_GROUP_ID')
+
+    # TODO add argparse filename, message
+
+    public_vk(
+        token=vk_access_token,
+        group_id=vk_group_id,
+        album_id=vk_album_id,
+        file_name=filename,
+        message=message,
+    )
+    public_telegram(
+        token=telegram_bot_token,
+        chat_id=telegram_chat_id,
+        file_name=filename,
+        message=message,
+    )
+    post_facebook(
+        token=fb_access_token,
+        group_id=fb_group_id,
+        file_name=filename,
+        message=message,
+    )
 
 
 if __name__ == "__main__":
